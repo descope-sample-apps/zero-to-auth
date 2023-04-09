ac19c7d75a0e8898dff95deaba7da03636a795ee
Asaf Shen <asaf@descope.com>
2023-04-09 19:00:32 +0300

    Create node sdk client

--- server/index.ts
+++ server/index.ts
@@ -5,8 +5,21 @@ import barChart from "./data/barChart.ts";
 import pieChart from "./data/pieChart.ts";
 import cors from "cors";
 import dotenv from "dotenv";
+import DescopeClient from "@descope/node-sdk";
 
 dotenv.config();
+
+if (!process.env.DESCOPE_PROJECT_ID) {
+  console.warn(
+    `Please set DESCOPE_PROJECT_ID in your environment variables, falling back to author's project id.`
+  );
+}
+
+const clientAuth = {
+  auth: DescopeClient({
+    projectId: process.env.DESCOPE_PROJECT_ID || "P2O9zUpunOAGLdVHie8He79diqHU",
+  }),
+};
 
 const app = express();
 app.use(
