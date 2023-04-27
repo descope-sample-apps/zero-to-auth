3ab2e28009935720c4ee46ed07fe07c3d13bdcf5
Kevin Gao <kevinjamesgao@gmail.com>
2023-04-27 11:43:29 -0700

    added README

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,10 @@ import React from "react";
 
 const App = () => {
   return (
-    <AuthProvider projectId="<Insert Project ID here>" sessionTokenViaCookie>
+    <AuthProvider
+      projectId="P2Of1JkkbUkaw9Gv17xsjZcPOf5o"
+      sessionTokenViaCookie
+    >
       <div className="App">
         <Container />
       </div>
