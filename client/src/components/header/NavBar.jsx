0c9a456d0473c1ffa6ca286553ad017f90f473bc
Asaf Shen <asaf@descope.com>
2023-05-16 00:55:54 +0300

    remove nav bar

--- client/src/components/header/NavBar.jsx
+++ client/src/components/header/NavBar.jsx
@@ -2,7 +2,6 @@ import { Avatar, Breadcrumb, Col, Popover, Row, Typography } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
   const content = (
