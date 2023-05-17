4a2e8ca42023da89fb530388c5d2845948a2cf68
Asaf Shen <asaf@descope.com>
2023-05-17 16:37:30 +0300

    add logout

--- client/src/components/header/NavBar.jsx
+++ client/src/components/header/NavBar.jsx
@@ -11,11 +11,15 @@ import { UserOutlined } from "@ant-design/icons";
 import "./navbar.scss";
 import { Link, useNavigate } from "react-router-dom";
 import { useCallback } from "react";
+import { useDescope } from "@descope/react-sdk";
 
 const NavBar = () => {
   const navigate = useNavigate();
 
+  const sdk = useDescope();
+
   const handleLogout = useCallback(() => {
+    sdk.logout();
     navigate("/login");
   }, [navigate]);
   return (
